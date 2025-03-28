d4efd7e5861e5b4cc1e87358fdfd58b83513bb87
Tung Ngo <tungthanh254@gmail.com>
2025-03-28 14:33:15 +0700

    setup frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "@/index.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-full bg-black">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
