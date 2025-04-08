01e96d92747f80bc0ccdab4a826dc7e4cf5d79fd
Tung Ngo <tungthanh254@gmail.com>
2025-04-08 16:05:16 +0700

    minor fix

--- src/pages/ProductDetailPage/components/BookCard.tsx
+++ src/pages/ProductDetailPage/components/BookCard.tsx
@@ -2,19 +2,27 @@ import RatingStar from "@/components/ui/Rating";
 import { Product } from "@/types/product";
 
 const BookCard = ({ product }: { product: Product }) => {
-    return (
-        <div className=" border border-[#EBEBF0] rounded-lg bg-white w-full h-[230px] flex flex-col gap-y-2 ">
-            <div className="w-full flex justify-center">
-                <img className="w-32.5 h-32.5 rounded-t-lg" src={product.images[0].base_url} alt="book image" />
-            </div>
-            <div className="px-2 h-11.5">
-                <p className="text-xs line-clamp-2">{product.name}</p>
-                <div className="pt-1">
-                    <RatingStar numofStar={5} />
-                </div>
-            </div>
-            <p className="text-sm p-2">{product.original_price}đ</p>
+  return (
+    <div className="flex h-[230px] w-full flex-col gap-y-2 rounded-lg border border-[#EBEBF0] bg-white">
+      <div className="flex w-full justify-center">
+        <div
+          className="relative flex h-32.5 w-32.5 justify-center bg-cover bg-center"
+          style={{ backgroundImage: `url(${product.images[0].base_url})` }}
+        />
+      </div>
+      <div className="relative h-11.5 px-2">
+        <p className="line-clamp-2 text-xs">{product.name}</p>
+        <div className="pt-1">
+          <RatingStar numofStar={5} />
         </div>
-    )
-}
+      </div>
+      <p className="p-2 text-sm relative">
+        {product.original_price.toLocaleString("vi-VN")}
+        <span className="absolute top-2 text-[10px] underline underline-offset-1">
+          đ
+        </span>
+      </p>
+    </div>
+  );
+};
 export default BookCard;
