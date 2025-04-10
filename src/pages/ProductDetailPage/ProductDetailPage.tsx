b4c9d96f838d89ca8ba51f57fe7c91009604d781
viet <viethbhn2003@gmail.com>
2025-04-10 22:55:49 +0700

    connect detail page api

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -7,12 +7,12 @@ import { useEffect, useState } from "react";
 import { Product } from "@/types/product";
 import { useParams } from "react-router-dom";
 
-
 const ProductDetailPage = () => {
   const { id } = useParams<{ id: string }>();
   const [product ,setProduct]=useState<Product>();
+  const { products, handleFetchAllProduct } = useProductStore();
 
-  const { products, handleGetProductById } = useProductStore();
+  const { productlist, handleGetProductById } = useProductStore();
   async function getProductDetail(productId: string|undefined) {
     try {
       const data = await handleGetProductById(productId);
@@ -23,31 +23,33 @@ const ProductDetailPage = () => {
     }
   }
   useEffect( () => {
+    handleFetchAllProduct()
     getProductDetail(id);
-  }, []);
+  }, [id]);
+  if(product)
   return (
     <div className="bg-background">
       <BreadCrumb />
       <div className="container mx-auto hidden pb-10 sm:gap-2 lg:flex lg:gap-4 xl:gap-6">
-        <BookImage />
-        <ProductInformation products={products} />
-        <Payment />
+        <BookImage product={product} />
+        <ProductInformation products={products} product={product} />
+        <Payment product={product} />
       </div>
 
       <div className="relative container mx-auto hidden px-2 pb-10 sm:flex sm:gap-2 md:flex lg:hidden lg:gap-4 xl:gap-6">
         <div className="sticky top-0 flex h-fit w-2/5 flex-col sm:gap-2 lg:gap-4 xl:gap-6">
-          <BookImage />
-          <Payment />
+          <BookImage product={product} />
+          <Payment product={product} />
         </div>
         <div className="w-3/5">
-          <ProductInformation products={products} />
+          <ProductInformation products={products} product={product} />
         </div>
       </div>
 
       <div className="container mx-auto flex flex-col items-center px-2 pb-10 sm:hidden sm:gap-2 md:hidden lg:hidden lg:gap-4 xl:gap-6">
-        <BookImage />
-        <Payment />
-        <ProductInformation products={products} />
+        <BookImage product={product} />
+        <Payment product={product} />
+        <ProductInformation products={products} product={product} />
       </div>
     </div>
   );
--- src/pages/ProductDetailPage/components/BookImage.tsx
+++ src/pages/ProductDetailPage/components/BookImage.tsx
@@ -1,65 +1,93 @@
 import { arrow_right, book_info } from "@/assets/icons/detail_page_icons";
-import { useState } from "react";
+import { Product } from "@/types/product";
+import { useEffect, useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 // import products from "@/assets/fakeData";
+const ITEMS_PER_PAGE = 6;
 
-const BookImage = () => {
-  const [isFront, setIsFront] = useState(true);
-  const [isFrontImg, setIsFrontImg] = useState(false);
+const BookImage = ({ product }: { product: Product }) => {
+  const [isMainImg, setIsMainImg] = useState(product.images[0]._id);
+  const [isThumbnailImg, setIsThumbnailImg] = useState(product.images[0]._id);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const maxIndex = Math.ceil(product.images.length / ITEMS_PER_PAGE) - 1;
 
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev < maxIndex ? prev + 1 : 0));
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev > 0 ? prev - 1 : maxIndex));
+  };
+  useEffect( () => {
+    setIsMainImg(product.images[0]._id);
+  }, [product]);
   return (
     <div className="md:max-w-100 w-full h-fit flex-1 gap-y-4 rounded-lg bg-white py-4 pb-0">
       <div className="mb-4 px-4">
         {/* Main Image */}
         <div className="mb-2 flex justify-center rounded-lg border border-gray-300">
-          {isFront ? (
-            <>
+          {product.images.map((img) => (
+            isMainImg === img._id ? <>
               <img
-                src="https://salt.tikicdn.com/cache/750x750/ts/product/17/4a/65/b4765d60127ee4cccf8fd551633fafd4.png.webp"
-                alt="ChatGPT Thực Chiến"
-                className="w-92 rounded-lg"
+                src={img.base_url}
+                alt={img.label ? img.label : ""}
+                className="w-92 h-92 rounded-lg"
               />
-            </>
-          ) : (
-            <>
-              <img
-                src="https://salt.tikicdn.com/cache/750x750/ts/product/a1/61/c8/0ffd6d2fd86a19ea9b5f2048fc2d0e5d.png.webp"
-                alt="ChatGPT Thực Chiến"
-                className="w-92 rounded-lg"
-              />
-            </>
-          )}
+            </> : ""
+          ))}
+
         </div>
 
         {/* Thumbnail Section */}
-        <div className="flex gap-2">
-          <img
-            src="https://salt.tikicdn.com/cache/100x100/ts/product/17/4a/65/b4765d60127ee4cccf8fd551633fafd4.png.webp"
-            alt="Thumbnail 1"
-            className={`w-13.5 cursor-pointer rounded-sm ${isFrontImg ? "border-2 border-[#0A68FF]" : "border border-[#EBEBF0]"}`}
-            onClick={() => setIsFrontImg(true)}
-            onMouseOver={() => setIsFront(true)}
-          />
-          <img
-            src="https://salt.tikicdn.com/cache/100x100/ts/product/a1/61/c8/0ffd6d2fd86a19ea9b5f2048fc2d0e5d.png.webp"
-            alt="Thumbnail 2"
-            className={`w-13.5 cursor-pointer rounded-sm ${isFrontImg ? "border border-[#EBEBF0]" : "border-2 border-[#0A68FF]"}`}
-            onClick={() => setIsFrontImg(false)}
-            onMouseOver={() => setIsFront(false)}
-          />
+        <div className="relative overflow-hidden">
+          <div className="flex gap-2 transition-transform duration-500"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+
+            {product.images.map((img) => (
+              <img
+                src={img.base_url}
+                alt="Thumbnail 1"
+                className={`w-13.5 cursor-pointer rounded-sm ${isThumbnailImg === img._id ? "border-2 border-[#0A68FF]" : "border border-[#EBEBF0]"}`}
+                onClick={() => setIsThumbnailImg(img._id)}
+                onMouseOver={() => setIsMainImg(img._id)}
+              />
+            ))}
+          </div>
+          {/* Nút điều hướng trái */}
+          {currentIndex > 0 && (
+            <button
+              onClick={prevSlide}
+              className="absolute top-1/2 left-2 flex h-8 w-8 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-white shadow"
+            >
+              <IoIosArrowBack size={24} color="#0A68FF" />
+            </button>
+          )}
+
+          {/* Nút điều hướng phải */}
+          {currentIndex < maxIndex && (
+            <button
+              onClick={nextSlide}
+              className="absolute top-1/2 right-2 flex h-8 w-8 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-white shadow"
+            >
+              <IoIosArrowForward size={24} color="#0A68FF" />
+            </button>
+          )}
+
+
         </div>
       </div>
 
       {/* View More Section */}
       <div className="flex justify-between border-t border-[#EBEBF0] p-4">
         <div className="flex items-center gap-x-1">
-          <img src={book_info} alt="" />
+          <img src={book_info} alt="book info" />
           <p className="text-sm">
             <span className="text-[#808089]">Xem thêm</span> Tóm tắt nội dung
             sách
           </p>
         </div>
-        <img src={arrow_right} alt="" />
+        <img src={arrow_right} alt="arrow right" />
       </div>
     </div>
   );
