db3646872ee7b19ed8d3564f0747d3cf80cde6a4
viet <viethbhn2003@gmail.com>
2025-04-10 14:03:51 +0700

    add profile page icons

---
+++ src/assets/icons/profile_page_icons/indext.ts
@@ -0,0 +1,8 @@
+import user_icon from "./user_icon.svg";
+import bell_icon from "./bell_icon.svg";
+import order_icon from "./order_icon.svg";
+import now_icon from "./now_icon.svg";
+
+export {
+  user_icon,bell_icon, order_icon, now_icon
+};
