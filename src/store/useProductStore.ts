9f7bb4ec43be66188f0ab837bd88943e78ea9fd7
Tong Tuan <ttong6913@gmail.com>
2025-04-10 15:32:00 +0700

    add getProduct by Id

--- src/store/useProductStore.ts
+++ src/store/useProductStore.ts
@@ -11,13 +11,39 @@ interface ProductStore {
     products:Product[];
     loading: boolean;
     handleFetchAllProduct: () => Promise<void>;
+    handleGetProductById:(id:string|undefined)=>Promise<Product|undefined>;
+    handleGetProductByCategory:(categoryName:string)=>Promise<void>;
 }
 export const useProductStore = create<ProductStore>((set) => ({
+    
     products: [],
     loading: false,
     handleFetchAllProduct: async () => {
         try {
             const response = await axiosInstance.get("/products");
+          set({ products: response.data.products });
+        } catch (error) {
+          const axiosError = error as AxiosError<ErrorResponse>;
+          toast.error(axiosError.response?.data?.message ?? "An error occurred");
+        } finally {
+          set({ loading: false });
+        }
+      },
+      handleGetProductById: async (id:string|undefined) => {
+        try {
+            const response = await axiosInstance.get(`/products/${id}`);
+            const product:Product=await response.data;
+            return product;
+        } catch (error) {
+          const axiosError = error as AxiosError<ErrorResponse>;
+          toast.error(axiosError.response?.data?.message ?? "An error occurred");
+        } finally {
+          set({ loading: false });
+        }
+      },
+      handleGetProductByCategory: async (categoryName:string) => {
+        try {
+            const response = await axiosInstance.get(`/products/category/${categoryName}`);
           set({ products: response.data.products });
         } catch (error) {
           const axiosError = error as AxiosError<ErrorResponse>;
