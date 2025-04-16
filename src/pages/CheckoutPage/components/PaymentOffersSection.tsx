db7c1473297c2bc35e37e3f76a305a4bba2f2bf5
Tong Tuan <ttong6913@gmail.com>
2025-04-16 15:20:59 +0700

    responsive checkout

--- src/pages/CheckoutPage/components/PaymentOffersSection.tsx
+++ src/pages/CheckoutPage/components/PaymentOffersSection.tsx
@@ -2,7 +2,20 @@ import { percentage, bank_logo } from "@/assets/icons/checkout_page_icons";
 import { tiki_pay } from "@/assets/icons/footer_icons";
 
 import OfferCard from "./OfferCard";
-
+const offerCard = [
+  "Thẻ Shinhan Platinum",
+  "Thẻ Shinhan Classic",
+  "Đơn từ 200k",
+  "Đơn từ 300k",
+  "Đơn từ 300k",
+  "Đơn từ 500k",
+  "Đơn từ 3700k",
+  "Đơn từ 1 triệu",
+  "Đơn từ 200k",
+  "Đơn từ 500k",
+  "Đơn từ 500k",
+  "TikiCARD",
+]
 const PaymentOffersSection = () => {
   return (
     <div className="bg-background p-4">
@@ -13,43 +26,12 @@ const PaymentOffersSection = () => {
         </span>
       </div>
 
-      <div className="mt-2 grid grid-cols-3 gap-2">
-        <OfferCard title="Freeship" logo={bank_logo}>
-          Thẻ Shinhan Platinum
-        </OfferCard>
-        <OfferCard title="Freeship" logo={bank_logo}>
-          Thẻ Shinhan Classic
-        </OfferCard>
-        <OfferCard title="Giảm 30k" logo={bank_logo}>
-          Đơn từ 200k
-        </OfferCard>
-        <OfferCard title="Giảm 50k" logo={bank_logo}>
-          Đơn từ 300k
-        </OfferCard>
-        <OfferCard title="Giảm 50k" logo={bank_logo}>
-          Đơn từ 300k
-        </OfferCard>
-        <OfferCard title="Giảm 70k" logo={bank_logo}>
-          Đơn từ 500k
-        </OfferCard>
-        <OfferCard title="Giảm 100k" logo={bank_logo}>
-          Đơn từ 3700k
-        </OfferCard>
-        <OfferCard title="Giảm 150k" logo={bank_logo}>
-          Đơn từ 1 triệu
-        </OfferCard>
-        <OfferCard title="Giảm 30k" logo={bank_logo}>
-          Đơn từ 200k
-        </OfferCard>
-        <OfferCard title="Giảm 50k" logo={bank_logo}>
-          Đơn từ 300k
-        </OfferCard>
-        <OfferCard title="Giảm 70k" logo={bank_logo}>
-          Đơn từ 500k
-        </OfferCard>
-        <OfferCard title="Freeship" logo={tiki_pay}>
-          TikiCARD
-        </OfferCard>
+      <div className=" mt-2 grid grid-cols-1 overflow-y-auto lg:grid-cols-3 max-h-48 lg:max-h-full md:grid-cols-2 gap-2">
+        {offerCard.map((item,index) => (
+          <OfferCard title="Freeship" logo={bank_logo} key={index}>
+            {item}
+          </OfferCard>
+        ))}
       </div>
     </div>
   );
